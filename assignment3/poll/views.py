71861f7ab467ae6a3860ff85739cb280106ea74c
Cory Hamelehle <chamelehle@mail.csuchico.edu>
2016-10-03 15:36:48 -0700

    text

--- assignment3/poll/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.http import HttpRequest
-from django.shortcuts import render
-from django.template import RequestContext
-
-def index(request):
-    assert isinstance(request, HttpRequest)
-    return render(request, 'helloWorld.html',)
